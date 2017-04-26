e0fc7feaaf499296b9e03249607564c9cf41538e
Artsem <arthemg@gmail.com>
2017-04-26 14:08:11 -0700

    Initial Commit

---
+++ src/interpreter/bytecode/debugbytecode/DebugLineCode.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interpreter.bytecode.debugbytecode;
+
+import interpreter.VirtualMachine;
+import interpreter.debugger.DebuggerVM;
+import interpreter.bytecode.ByteCode;
+import java.util.*;
+/**
+ *
+ * @author Artsem
+ */
+public class DebugLineCode extends ByteCode 
+{
+    int currentLine;
+
+    public DebugLineCode() 
+    {
+        super("DebugLineCode");
+    }
+
+    @Override
+    public void execute(VirtualMachine vm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void init(Vector<String> args) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String dumpOut(VirtualMachine vm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
