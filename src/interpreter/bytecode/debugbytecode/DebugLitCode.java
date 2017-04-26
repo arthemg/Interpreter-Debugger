67f4b377523871aea07f82363f4a97b776d63806
Artsem <arthemg@gmail.com>
2017-04-26 14:07:32 -0700

    Initial Commit

---
+++ src/interpreter/bytecode/debugbytecode/DebugLitCode.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interpreter.bytecode.debugbytecode;
+
+import interpreter.VirtualMachine;
+import interpreter.debugger.DebuggerVM;
+import interpreter.bytecode.LitCode;
+import java.util.*;
+/**
+ *
+ * @author Artsem
+ */
+public class DebugLitCode extends LitCode
+{
+    public DebugLitCode()
+    {
+        this.byteCodeName = "DebugLitCode";
+    }
+}
