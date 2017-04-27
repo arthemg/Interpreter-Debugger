796a2c3a1637f1d9d3647c1eeb796423ab97abe6
Artsem <Artsem@192.168.1.119>
2017-04-26 21:07:21 -0700

    Implemented DebugReturnCode

--- src/interpreter/bytecode/debugbytecode/DebugReturnCode.java
+++ src/interpreter/bytecode/debugbytecode/DebugReturnCode.java
@@ -4,8 +4,9 @@
  * and open the template in the editor.
  */
 package interpreter.bytecode.debugbytecode;
+
 import interpreter.bytecode.ReturnCode;
-import java.util.*;
+import interpreter.VirtualMachine;
 import interpreter.debugger.DebuggerVM;
 
 /**
@@ -17,5 +18,16 @@ public class DebugReturnCode extends ReturnCode
    public DebugReturnCode()
    {
        this.byteCodeName = "DebugReturnCode";
+   }
+   
+   @Override
+   public void execute(VirtualMachine vm)
+   {
+       super.execute(vm);
+       
+       //Cast VirtualMachine into DebuggerVM
+        DebuggerVM debugVM = (DebuggerVM) vm;
+        
+        debugVM.popFctEnvRecord();
    }
 }
