1349c7e8e98a8e46c96efd05079ddcaf26b62f58
Artsem <Artsem@192.168.1.119>
2017-04-26 21:07:02 -0700

    Intial Commit With Stumps

--- src/interpreter/debugger/DebuggerVM.java
+++ src/interpreter/debugger/DebuggerVM.java
@@ -13,5 +13,41 @@ import interpreter.VirtualMachine;
  */
 public class DebuggerVM extends VirtualMachine
 {
+
+    public void pushSymbolOffset(String variable, int offset) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setFunctionInfo(String functionName, int firstLine, int lastLine) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void popCurrentFct() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void newFctEnvRecord() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setCurrentFct() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setCurrentLineNumber() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void pushSymbol() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
+    public void doPop(int val)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void popFctEnvRecord() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
