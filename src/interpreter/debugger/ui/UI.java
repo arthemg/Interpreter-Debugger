4f84b1beeb7b59d39fa1d84b1d150466de5567b4
Artsem <Artsem@192.168.1.119>
2017-04-27 21:24:00 -0700

    Empty UI for the user interaction with debugger

---
+++ src/interpreter/debugger/ui/UI.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interpreter.debugger.ui;
+
+import interpreter.Program;
+import java.util.HashSet;
+
+/**
+ *
+ * @author Artsem
+ */
+public class UI {
+
+    public UI(Program program, String sourceFile, HashSet<Integer> breakPoints) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
