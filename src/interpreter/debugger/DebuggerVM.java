0227e41a078f8628443c2d7e050f63a062de323d
Artsem <Artsem@192.168.1.119>
2017-04-27 21:20:13 -0700

    Implemented most of the DebugVm

--- src/interpreter/debugger/DebuggerVM.java
+++ src/interpreter/debugger/DebuggerVM.java
@@ -5,49 +5,169 @@
  */
 package interpreter.debugger;
 
-import interpreter.VirtualMachine;
+import interpreter.*;
+import java.io.*;
+import java.util.*;
 
 /**
  *
  * @author Artsem
  */
-public class DebuggerVM extends VirtualMachine
+
+/**
+ * bpTracker is used to group 2 fields the line of the source code and track
+ * if there is a breakpoint set at that line similar to the SymbolTable
+ *
+ */
+class bpTracker
 {
+    boolean breakPoint;
+    String sourceLine;
+    
+    public bpTracker(String sourceLine, boolean breakPoint)
+    {
+        this.sourceLine = sourceLine;
+        this.breakPoint = breakPoint;
+    }
+    
+    public void setBreakPointBool()
+    {
+        breakPoint = true;
+    }
+    
+    public boolean getBreakPointBool()
+    {
+        return this.breakPoint;
+    }
+    
+    public void setSourceLine(String line)
+    {
+        this.sourceLine = line;
+    }
+    
+    public String getSourceLine()
+    {
+        return this.sourceLine;
+    }
+    
+    public void unsetBreakPointBool()
+    {
+        breakPoint = false;
+    }
+    
+}
 
-    public void pushSymbolOffset(String variable, int offset) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+public class DebuggerVM extends VirtualMachine
+{   
+    BufferedReader sourceFile;
+    Stack<FunctionEnvironmentRecord> fctEnvStack;
+    HashSet<Integer> breakPoints;
+    FunctionEnvironmentRecord topFctEnvRecord;
+    Vector<bpTracker> sourceLineBpTracker;
+    boolean enteredFct;
+    boolean stepOutPending;
+    boolean stopDebugger;
+    
+    public DebuggerVM(Program program, String sourceFile, HashSet<Integer> breakPoints) throws IOException, FileNotFoundException
+    {
+        super(program);
+        //Initialize all the variables
+        topFctEnvRecord = new FunctionEnvironmentRecord();
+        sourceLineBpTracker = new Vector<>();
+        this.breakPoints = breakPoints;
+        
+        FunctionEnvironmentRecord constructorFctEnvRecord = new FunctionEnvironmentRecord();
+        constructorFctEnvRecord.setStartLine(1);
+        int endLine = sourceLineBpTracker.size();
+        constructorFctEnvRecord.setEndLine(endLine);
+        fctEnvStack.push(constructorFctEnvRecord);
+        
+        //Read source file and initialize all the source lines and breakpoints in a vector
+        this.sourceFile = new BufferedReader(new FileReader(sourceFile));
+        String currentString = this.sourceFile.readLine();
+        
+        while(currentString != null)
+        {
+            bpTracker currentStringTrack = new bpTracker(currentString, false);
+            sourceLineBpTracker.add(currentStringTrack);
+        }
     }
 
-    public void setFunctionInfo(String functionName, int firstLine, int lastLine) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void pushSymbolOffset(String variable, int offset) 
+    {
+        if(!fctEnvStack.empty())
+        {
+            FunctionEnvironmentRecord topFctEnvRecord = fctEnvStack.peek();
+            topFctEnvRecord.setVarVal(variable, offset);
+        }
     }
 
-    public void popCurrentFct() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setFunctionInfo(String functionName, int firstLine, int lastLine) 
+    {
+        if(!fctEnvStack.empty())
+        {
+            fctEnvStack.peek().setFunctionInfo(functionName, lastLine, lastLine);
+        }
+        popCurrentFct();
+        if(firstLine > 0)
+        {   
+            //if we hit set break point we stop
+            if(sourceLineBpTracker.get(firstLine-1).breakPoint)
+            {
+                super.halt();
+            }
+        }
     }
 
-    public void newFctEnvRecord() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void popCurrentFct()
+    {
+        this.enteredFct = false;
+    }
+    
+    public void setCurrentFct() 
+    {
+        this.enteredFct = true;
+    }
+    
+    public void newFctEnvRecord() 
+    {
+        FunctionEnvironmentRecord newFctEnv = new FunctionEnvironmentRecord();
+        fctEnvStack.push(newFctEnv);
     }
 
-    public void setCurrentFct() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setCurrentLineNumber(int currentLine) 
+    {
+        if(!fctEnvStack.empty())
+        {
+            fctEnvStack.peek().setCurrentLineNumber(currentLine);
+        }
+        
     }
 
-    public void setCurrentLineNumber() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public void pushSymbol() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void pushSymbol(String symbol) 
+    {
+        if(!fctEnvStack.empty())
+        {
+            int stackPointer = this.runStack.getStackPointer();
+            stackPointer--;
+            FunctionEnvironmentRecord topEnvironmentRecord = fctEnvStack.peek();
+            topEnvironmentRecord.setVarVal(symbol, stackPointer);
+        }
     }
     
     public void doPop(int val)
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(!fctEnvStack.empty())
+        {
+            fctEnvStack.peek().doPop(val);
+        }
     }
 
-    public void popFctEnvRecord() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void popFctEnvRecord() 
+    {
+       if(!fctEnvStack.empty())
+       {
+           fctEnvStack.pop();
+       }
     }
 }
